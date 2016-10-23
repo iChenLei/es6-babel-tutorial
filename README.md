564358b395887a30f1dd2980185a3409f9effc25
chenlei <2470828450@qq.com>
2016-10-23 22:04:38 +0800

    let and const

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 ##ES6 training..
-'''javascript
-    const js = "ES6";
-    class front_end extend ES6{
-        //coding...
-    }
-'''
---
+++ letandconst.js
@@ -0,0 +1,18 @@
+‘use strict’; //严格模式
+{
+    let a = 10;
+    var b = 1;
+}
+
+// babel transelate let to var
+
+let //不存在变量提升
+    //先声明后使用
+
+typeof  //会出现错误
+
+let //在作用域内不能多次声明变量
+
+const //只读变量，不能修改 babel transelate it to var (right now)
+
+
