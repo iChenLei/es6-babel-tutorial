bf634aac51e8b787777759edf23c777e91c064c6
chenlei <2470828450@qq.com>
2016-10-27 23:22:17 +0800

    new math function

---
+++ mathofes6.js
@@ -0,0 +1,13 @@
+//0b表示二进制
+console.log(0b111110111);
+//0o表示八进制
+console.log(0o767);
+//number方法得到十进制
+console.log(Number("0b111"));
+//判断数字
+console.log(Number.isNaN("20"));
+Number.parseInt("122sss");
+
+console.log(Number.isInteger(23));
+
+console.log(2**2); //babel to console.log(Math.pow(2,2))
