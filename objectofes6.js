24b3d364de428a38c801f5bd235fb680bffdae38
chenlei <2470828450@qq.com>
2016-10-31 23:27:19 +0800

    new object properties

---
+++ objectofes6.js
@@ -0,0 +1,13 @@
+var foo = 'bar';
+var baz = {foo};
+
+var person = {
+    name:"zhangsan",
+    birth,
+    hello(){
+        console.log("my name is",this.name);
+    }
+};
+
+module.exports = {getItem,setItem,clear};
+
