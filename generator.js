54c34d9411a58131c5293dbc6f02a94b452c3d58
chenlei <2470828450@qq.com>
2016-11-03 23:25:40 +0800

    generator function*

---
+++ generator.js
@@ -0,0 +1,31 @@
+function* f(){
+  yield console.log('执行了');
+  console.log('再次执行');
+}
+
+f().next();
+
+function* dataConsumer() {
+  console.log('Started');
+  console.log(`1. ${yield}`);
+  console.log(`2. ${yield}`);
+  return 'result';
+}
+
+let genObj = dataConsumer();
+genObj.next();
+genObj.next('a')
+genObj.next('b')
+
+function *foo() {
+  yield 1;
+  yield 2;
+  yield 3;
+  yield 4;
+  yield 5;
+  return 6;
+}
+
+for (let v of foo()) {
+  console.log(v);
+}
